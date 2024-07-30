198e6c0349f578ceb68acac654537f9567f03e09
yeachan <dus001228@naver.com>
2024-07-31 01:06:16 +0900

    feat: 댓글 모델 생성 #13

--- models.py
+++ models.py
@@ -1,5 +1,7 @@
 from sqlalchemy import Column, Integer, VARCHAR, DateTime, ForeignKey, Float
 from datetime import datetime
+from sqlalchemy.orm import relationship
+
 
 from database import Base
 
@@ -9,7 +11,7 @@ class User(Base):
   user_no = Column(Integer, primary_key=True, autoincrement=True)
   user_name = Column(VARCHAR(10), nullable=False)
   phone= Column(VARCHAR(100), nullable=False, unique=True)
-  birth= Column(VARCHAR(100), nullable=False, unique=True)
+  birth= Column(VARCHAR(100), nullable=False)
   hashed_pw=Column(VARCHAR(100), nullable=False)
   role=Column(VARCHAR(20), nullable=False, default='MEMBER')
   family_id = Column(VARCHAR(20), ForeignKey('Families.id'), default="None") 
@@ -24,12 +26,29 @@ class Family(Base):
 
 
 class Family_Photo(Base):
-  __tablename__ = 'photos'
+    __tablename__ = 'photos'
 
-  file = Column(VARCHAR, nullable=False, primary_key=True)
-  author = Column(VARCHAR(10), nullable=False)
-  regdate = Column(DateTime, nullable=False, default=datetime.now)
-  family_id = Column(VARCHAR(20), ForeignKey('Families.id'), default="None") 
-  comment = Column(VARCHAR(20), nullable=True, default="None")
-  positive = Column(Float, default=0)
-  negative = Column(Float, default=0)
+    photo_no = Column(Integer, primary_key=True, autoincrement=True)
+    file = Column(VARCHAR, nullable=False)
+    author = Column(VARCHAR(10), nullable=False)
+    regdate = Column(DateTime, nullable=False, default=datetime.now)
+    family_id = Column(VARCHAR(20), ForeignKey('Families.id'), default="None") 
+    comment = Column(VARCHAR(20), nullable=True, default="None")
+    positive = Column(Float, default=0)
+    negative = Column(Float, default=0)
+
+    # 댓글 관계 추가
+    comments = relationship("Family_Photo_comment", back_populates="photo")
+
+class Family_Photo_comment(Base):
+    __tablename__ = 'photo_comments'
+
+    comment_id = Column(Integer, primary_key=True, autoincrement=True)  # 댓글 ID
+    photo_no = Column(VARCHAR, ForeignKey('photos.photo_no'), nullable=False)  # 댓글이 달린 사진의 파일명
+    user_name = Column(Integer, ForeignKey('Users.user_name'), nullable=False)  # 댓글 작성자
+    comment = Column(VARCHAR(255), nullable=False)  # 댓글 내용
+    regdate = Column(DateTime, nullable=False, default=datetime.now)  # 댓글 작성일
+
+    # 관계 설정 (옵션)
+    photo = relationship("Family_Photo", back_populates="comments")
+    user = relationship("User")
