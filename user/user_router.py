49945cd7202b617c3025757ba9e204e3a1195e7a
yeachan <dus001228@naver.com>
2024-07-30 16:01:33 +0900

    fix: 쿠키 통신 에러 해결

--- user/user_router.py
+++ user/user_router.py
@@ -91,7 +91,7 @@ async def login(response: Response, login_form: OAuth2PasswordRequestForm = Depe
     access_token = create_access_token(data={"sub": user.phone}, expires_delta=access_token_expires)
 
     # 쿠키에 저장
-    response.set_cookie(key="access_token", value=access_token, expires=access_token_expires, httponly=True)
+    response.set_cookie(key="access_token", value=access_token, expires=access_token_expires, httponly=True, samesite="none", secure=True)
 
     if not res:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid user or password")
