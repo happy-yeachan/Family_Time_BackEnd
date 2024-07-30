bb6043ebcf0f984a4e21bb78db0fd5286b0614c4
yeachan <dus001228@naver.com>
2024-07-30 15:19:27 +0900

    feat: 가족 참가 기능 라우팅 #3

--- family/family_router.py
+++ family/family_router.py
@@ -4,8 +4,8 @@ from database import get_db
 
 from fastapi import APIRouter, Depends, status, HTTPException, Response, Request
 from jose import jwt, JWTError
-from family import family_crud, family_schema
-from user import user_crud, user_router, user_schema
+from family import family_crud
+from user import user_router, user_schema
 import random
 import string
 
@@ -45,20 +45,25 @@ async def create_family(name: str,  db: Session = Depends(get_db), phone: user_s
         characters = string.ascii_letters + string.digits
         random_string = ''.join(random.choice(characters) for _ in range(5))
         if not family_crud.get_family(random_string, db):
-            family = family_crud.create_Family(random_string, name, db, phone)
+            family_crud.create_Family(random_string, name, db, phone)
             break
 
 
-    return family, HTTPException(status_code=status.HTTP_200_OK, detail="create_family successful")
+    return HTTPException(status_code=status.HTTP_200_OK, detail="create_family successful")
 
 
 
 @app.get(path="/join")
-async def logout(response: Response, request: Request):
+async def join(id: str, db: Session = Depends(get_db), phone: user_schema.Current_User = Depends(get_current_phone)):
 
-    # 쿠키 삭제
-    response.delete_cookie(key="access_token")
+    if not family_crud.get_family(id, db):
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        raise credentials_exception
+    
+    family_crud.join_Family(id, phone, db)
 
-    return HTTPException(status_code=status.HTTP_200_OK, detail="Logout successful")
-
-
+    return HTTPException(status_code=status.HTTP_200_OK, detail="join successful")
